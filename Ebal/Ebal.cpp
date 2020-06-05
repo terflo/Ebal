0011c873135357995a628c18b54ae14b47d2b539
terflo <danil.krivoschiokov@yandex.ru>
2020-06-06 02:10:43 +0700

    123

--- Ebal/Ebal.cpp
+++ Ebal/Ebal.cpp
@@ -1,7 +1,7 @@
-﻿// Ebal.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
